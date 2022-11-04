e50b28d8542c56525d9b584c15efa3387565993f
aleksandraag <ola.golinska@vp.pl>
2022-11-04 07:01:27 +0100

    update

---
+++ js/introduction.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const name = "Ola";
+const age3 = 25;
+const favFood = "pizza, pasta";
+console.log(name);
+console.log(age);
+console.log(favFood);
+
+console.log("Hej, jestem " + name + " lubię " + favFood);
+
+console.log(`Hej, jestem ${name}`);
+
+
+const myName = 'Ola';
+const petName = 'Bobo';
+const boboAge = 18;
+console.log(`Cześć, jestem ${myName} a to jest moje ${petName}`);
+console.log("" + petName + " ma już " + boboAge + " lat!");
+
+
+//homework
+
+const firstName = 'Tomek'
+let age = 12
+age = 18
+ 
+const favColor = 'niebieski'
+const favMeal = 'schabowy'
+ 
+let currentCar
+currentCar = 'Audi'
+ 
+let firstName2 = 'Ania'
+let age2 = 24
+let favColor2 = "czerwony";
+
+
+
+// 1: favMeal, 2,6: currentCar, 3: const favColor, 4: firstName2, 5: favColor2, 
--- js/sentence.js
+++
@@ -1,6 +0,0 @@
-2 + 2
-
-console.log("dupa");
-
-console.log('lel');
-console.log(`brrrr`);
