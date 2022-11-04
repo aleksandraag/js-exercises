15e8283f51a499eca18de723ffc630909a6d22dd
aleksandraag <ola.golinska@vp.pl>
2022-11-04 05:54:21 +0100

    update

--- js/sentence.js
+++ js/sentence.js
@@ -1,49 +1,6 @@
-"use strict";
+2 + 2
 
-// function sum(a, b) {
-//     return a + b;
-// }
+console.log("dupa");
 
-// console.log(sum(2, 5));
-
-// function checkStorage(available, ordered) {
-//   // Change code below this line
-
-//   if (ordered === 0) {
-//     return "Your order is empty!";
-//   }
-//   if (ordered > available) {
-//     return "Your order is too large, not enough goods in stock!";
-//   }
-
-//   return "The order is accepted, our manager will contact you";
-// }
-
-// console.log(checkStorage(600, 100));
-
-// const fruits = ["apple", "plum", "pear", "orange"];
-
-// // Change code below this line
-// const firstElement = fruits[0];
-// const secondElement = fruits[1];
-// const lastElement = fruits[3];
-
-// console.log(firstElement);
-
-
-// const fruits = ["apple", "peach", "pear", "banana"];
-
-// Change code below this line
-// const lastElementIndex = fruits.length - 1;
-// const lastElement = fruits[lastElementIndex];
-
-// console.log(lastElement);
-
-
-function getExtremeElements(array) {
-  
-  return [array[0],array[(array.length) -1]]
-
-}
-
-console.log(getExtremeElements([1, 2, 3, 4, 5, 6]));
+console.log('lel');
+console.log(`brrrr`);
